18006bd68bee8b4526415bde6e3a3b0de5295831
Reeyan Khimani <reeyankhimani@Reeyans-MacBook-Air.local>
2023-03-10 09:00:03 -0500

    resume added

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import RtlLayout from "layouts/rtl";
 import AdminLayout from "layouts/admin";
 import AuthLayout from "layouts/auth";
 import Tech from "components/Tech";
+import Resume from "./combos/Resume";
  
 const App = () => {
  return (
@@ -31,7 +32,7 @@ const App = () => {
           <Tech />
         </>
       }/>
-
+      <Route path="/resume" element={<><Resume /></>}/>
       <Route path="auth/*" element={<AuthLayout />} />
       <Route path="admin/*" element={<AdminLayout />} />
       <Route path="rtl/*" element={<RtlLayout />} />
---
+++ client/src/combos/Resume.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../index.css";
+
+const Resume = () => {
+  return (
+    <>
+      <iframe src="https://reeyanresume.tiiny.site" frameborder="0" className="min-h-screen" width="100%"></iframe>
+    </>
+  );
+}
+
+export default Resume;
