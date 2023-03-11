5872363935b8e3fbc951c7eb67d78e05625a0844
Reeyan Khimani <reeyankhimani@Reeyans-Air.attlocal.net>
2023-03-10 22:20:49 -0500

    added blog and projects

--- client/src/combos/Heading.js
+++ client/src/combos/Heading.js
@@ -23,11 +23,12 @@ const Heading = () => {
                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
               </a>
               <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
+                <li><a href="/scieng">Science Fair</a></li>
+                <li><a href="https://visualizer.reeyan.me">Sort Visualizer</a></li>
               </ul>
             </li>
             <li><a href="https://linkedin.com/in/reeyank">Linkedin</a></li>
+            <li ><a href="https://blog.reeyan.me/" target="_blank">Blog</a></li>
           </ul>
         </div>
         <a className="btn btn-ghost normal-case text-xl" href="/">
@@ -43,11 +44,12 @@ const Heading = () => {
               <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
             </a>
             <ul className="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
+              <li><a href="/scieng">Science Fair</a></li>
+              <li><a href="https://visualizer.reeyan.me">Sort Visualizer</a></li>
             </ul>
           </li>
           <li ><a href="https://linkedin.com/in/reeyank/" target="_blank">Linkedin</a></li>
+          <li ><a href="https://blog.reeyan.me/" target="_blank">Blog</a></li>
         </ul>
       </div>
       <div className="navbar-end">
