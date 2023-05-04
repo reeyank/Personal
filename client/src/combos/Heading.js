63d5db23c4c71bb3f5208e52b87ffea64626c6bd
Reeyan Khimani <Khimanireeyan@gmail.com>
2023-05-04 18:14:25 +0000

    fix link to portfolio

--- client/src/combos/Heading.js
+++ client/src/combos/Heading.js
@@ -16,7 +16,7 @@ const Heading = () => {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
           <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a href="https://porfolio.reeyan.me">About</a></li>
+            <li><a href="https://portfolio.reeyan.me">About</a></li>
             <li tabIndex={0}>
               <a className="justify-between">
                 Projects
