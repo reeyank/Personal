2dca280342dc8f4718c4dfbf864c693f0ed82a6c
Reeyan Khimani <Khimanireeyan@gmail.com>
2023-05-04 15:54:11 +0000

    finish fixing the link

--- client/src/combos/Heading.js
+++ client/src/combos/Heading.js
@@ -39,8 +39,8 @@ const Heading = () => {
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li><a href="https://portfolio.reeeyan.me">About</a></li>
-          <li  tabIndex={0}>s
+          <li><a href="https://portfolio.reeyan.me">About</a></li>
+          <li  tabIndex={0}>
             <a>
               Projects
               <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
