533d68554cfe8990e7a74c8b902802c53d45c44b
Reeyan Khimani <Khimanireeyan@gmail.com>
2023-05-07 03:20:17 +0000

    adsense

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,8 @@ const App = () => {
   <div>
     <Helmet>
       <title>Reeyan's Portfolio Website</title>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9457672118073189"
+     crossorigin="anonymous"></script>
     </Helmet>
 
     <Routes>
